8f80d767064c9ed3de5388b01e8fe21a4d90f9e3
Joshua Khoo <khoo.joshua.316@gmail.com>
2019-12-03 20:13:31 -0700

    Store whole file

--- parser.py
+++ parser.py
@@ -1,15 +1,27 @@
 import chess.pgn
 from pymongo import MongoClient
+import time
 
-state = []
+start = time.time()
 
-pgn = open("example.pgn")
-game = chess.pgn.read_game(pgn)
-board = game.board()
-state.append(board.copy())
-for move in game.mainline_moves():
-    board.push(move)
+games = []
+
+pgn = open("lichess_db_standard_rated_2013-01.pgn")
+while True:
+    line = pgn.readline().strip()
+    if line == '':
+        break
+    state = []
+    game = chess.pgn.read_game(pgn)
+    board = game.board()
     state.append(board.copy())
+    for move in game.mainline_moves():
+        board.push(move)
+        state.append(board.copy())
+    games.append(state)
+
+end = time.time()
+print("Time to parse file: ", end - start)
 
 client = MongoClient("mongodb://34.70.135.10:27017")
 client.close()
